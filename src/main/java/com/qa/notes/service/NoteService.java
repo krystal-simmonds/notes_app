39dd6e37b3641ecbed42a39983fe87618530332e
Krystal <krystalsimm01@me.com>
2020-08-26 14:37:32 +0100

    Added CRUD functionality

--- src/main/java/com/qa/notes/service/NoteService.java
+++ src/main/java/com/qa/notes/service/NoteService.java
@@ -29,4 +29,21 @@ public class NoteService {
     public Note findNoteById(Long id){
         return this.repo.findById(id).orElseThrow(NoteNotFoundException::new);
     }
+
+    public Note updateNote(Long id, Note note){
+        Note update = findNoteById(id);
+        update.setTitle(note.getTitle());
+        update.setDescription(note.getDescription());
+        return this.repo.save(update);
+    }
+
+    public Boolean deleteNoteById(Long id){
+        if(!this.repo.existsById(id)){
+            throw new NoteNotFoundException();
+        }
+        this.repo.deleteById(id);
+        return this.repo.existsById(id);
+    }
+
+
 }
