176787d2d3e7f50f567655c02975237393770ea7
Krystal <krystalsimm01@me.com>
2020-08-26 14:22:09 +0100

    Added Notes Repo, Notes Service and Exceptions package

---
+++ src/main/java/com/qa/notes/service/NoteService.java
@@ -0,0 +1,32 @@
+package com.qa.notes.service;
+
+import com.qa.notes.domain.Note;
+import com.qa.notes.exceptions.NoteNotFoundException;
+import com.qa.notes.repo.NotesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+
+    private final NotesRepository repo;
+
+    @Autowired
+    public NoteService(NotesRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Note> readAllNotes() {
+        return this.repo.findAll();
+    }
+
+    public Note createNote(Note note){
+        return this.repo.save(note);
+    }
+
+    public Note findNoteById(Long id){
+        return this.repo.findById(id).orElseThrow(NoteNotFoundException::new);
+    }
+}
